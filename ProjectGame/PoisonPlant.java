11a99388ea629c5b8d58eb4ef9a7fe4ec01cf547
Chase <esmeraldarekz99@gmail.com>
2017-02-16 12:22:22 -0800

    Enemy Class
    3 different enemies being added

---
+++ ProjectGame/PoisonPlant.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class PoisonPlant here.
+ * 
+ * @author Esmeralda Lopez
+ * @version 2.16.17
+ */
+public class PoisonPlant extends Enemy{
+   
+}
---
+++ ProjectGame/Reaper.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class Reaper here.
+ * 
+ * @author Esmeralda Lopez
+ * @version 2.16.17
+ */
+public class Reaper extends Enemy{
+   
+}
