6e652453aa0b8f496f8dd9f4266e5209897fd78b
Chase <esmeraldarekz99@gmail.com>
2017-03-21 12:30:56 -0700

    save

--- ProjectGame/Vest.java
+++ ProjectGame/Vest.java
@@ -5,6 +5,9 @@
  * @author Esmeralda 
  * @version 2.27.17
  */
-public class Vest extends Armor
-{
+public class Vest extends Armor{
+    int defense = 500;
+    public Vest(){
+        super("Vest", 500);
+    }
 }
