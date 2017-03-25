608ebca8ebdd48fa05705f726fe7541019a1f863
Chase <esmeraldarekz99@gmail.com>
2017-03-24 23:50:14 -0700

    save

--- ProjectGame/Character.java
+++ ProjectGame/Character.java
@@ -5,23 +5,20 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public abstract class Character
-{ int hitpoints;
+public abstract class Character{
+    int hitpoints;
     String name;
     public Character(String name, int hp){
         hitpoints = hp;
         this.name = name;
     }
-
     public void damageTaken(int dmg){
         hitpoints -= dmg;
     }
-
     public String getName(){
         return name;   
-    }
-
+     }
     public int getHp(){
         return hitpoints;   
-    }
-}
+     }
+   }
